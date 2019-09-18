41ee65f5598bd366ea0772d7c7a28059152857c6
Hanien ElGalfy <hanienelgalfy@Haniens-MacBook-Air.local>
2019-09-18 23:41:53 +0200

    Storyboard done

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Hanien ElGalfy on 9/18/19.
+//  Copyright © 2019 Hanien ElGalfy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
